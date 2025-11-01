4f1227df4741e894ee8c8e42ed6f4a27716d7f57
WistantKode <wistantkode@protonmail.com>
2025-11-02 00:41:47 +0100

    feat

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
